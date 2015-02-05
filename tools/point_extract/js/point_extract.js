4e31ec019411190bdbcb0c2020b8dc5ea4021e4e
Gabriel Quiles-Perez <info@gabyquiles.com>
2015-02-05 16:12:44 -0500

    Extract data based on decimal coordinates

--- tools/point_extract/js/point_extract.js
+++ tools/point_extract/js/point_extract.js
@@ -13,6 +13,7 @@
             self.templateName = "point_extract";
             self.activate = function() {
                 var source_canvas = $("#image_to_analyze");
+                var extract_coords_btn = $("#extract_coords_button");
                 source_canvas.click(function(evt) {
                     var source_context = this.getContext("2d");
                     var offset = $(this).offset();
@@ -39,6 +40,31 @@
                         }
                     });
                 });
+                extract_coords_btn.click(function (evt) {
+                    var coords = $("#coords").val().split(/\n|\r/);
+                    $.ajax({
+                        url: "/?q=api/image/" + Drupal.settings.image_analyzer.img.fid + "/coords_extract.json",
+                        type: 'post',
+                        dataType: 'json',
+                        data: {coords: coords},
+                        beforeSend: function(){
+                            $('#point_extract_table tr:last').after('<tr class="loading"><td colspan="3">Loading</td></tr>');
+                        },
+                        complete: function(){
+                            $('.loading').remove();
+                        },
+                        success:  function(data, textStatus, xhr) {
+                            console.log(data)
+                            var data = jQuery.parseJSON(data[0]);
+                            for(i = 0; i < data.length; i++) {
+                                self.values.push({latitude: data[i].y, longitude: data[i].x, value: data[i].value});
+                            }
+                        },
+                        error:  function(data, textStatus, xhr) {
+                            alert("We had a communication problem and are unable to analyze the image at this moment.");
+                        }
+                    });
+                })
             }
 
         }
@@ -51,7 +77,13 @@
     }
 
     templateEngine.addTemplate("point_extract", "\
+    <p>You can click on any point on the image or enter the coordinates below to extract the corresponding values.</p>\
 		<div id=\"point_extract\"> \
+		    <div id=\"extract_coords\">\
+                <label for=\"coords\">Enter decimal coordinates (One coordinate by line in the format of lat,lon):</label>\
+                <textarea id=\"coords\" name=\"coords\" class=\"coords_input\"></textarea>\
+                <button id=\"extract_coords_button\" type='submit'>Get Data</button>\
+            </div>\
 			<table class=\"point_extract\"> \
 				<thead> \
 					<tr data-bind=\"foreach: columns\"> \
